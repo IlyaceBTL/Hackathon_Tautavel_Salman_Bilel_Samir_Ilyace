afa5304618c43d316cd73b07b463db57a96823b6
IlyaceBTL <ilyaceboutouil.pro@gmail.com>
2025-12-12 23:19:44 +0100

    (Ilyace) first commit

---
+++ demo/src/main/java/com/interarc/demo/InterArc42Application.java
@@ -0,0 +1,13 @@
+package com.interarc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterArc42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterArc42Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/interarc/demo/InterArc42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interarc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterArc42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
