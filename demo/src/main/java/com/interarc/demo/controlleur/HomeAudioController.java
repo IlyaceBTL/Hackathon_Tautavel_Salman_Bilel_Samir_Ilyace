402476e3b48bd5c6e8775ff3701381eba3f5a298
Kaiman <salman.aziev@outlook.fr>
2025-12-13 17:59:56 +0100

    (salman) voice progression

--- demo/src/main/java/com/interarc/demo/controlleur/HomeAudioController.java
+++ demo/src/main/java/com/interarc/demo/controlleur/HomeAudioController.java
@@ -1,12 +1,30 @@
 package com.interarc.demo.controlleur;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.interarc.demo.model.AnimalModel;
+import com.interarc.demo.services.AnimalService;
 
 @Controller
 public class HomeAudioController {
+
+    // 1. On déclare la variable sans lui donner de valeur (= null est supprimé)
+    private final AnimalService animalService;
+
+    @Autowired
+    // 2. Le constructeur porte maintenant le bon nom : HomeAudioController
+    public HomeAudioController(AnimalService animalService) {
+        this.animalService = animalService;
+    }
+
     @GetMapping("/audio")
-    public String home() {
-        return "frontend/home_page_audio";
+    public String home(Model model) {
+        // Cette partie était déjà correcte !
+        AnimalModel animal = animalService.getFirstAnimal();
+        model.addAttribute("animal", animal);
+        return "frontend/home_page_audio"; 
     }
 }
